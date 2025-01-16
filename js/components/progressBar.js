1d43cd6e947ca64e7c24d6fe83f19fb83e211955
fvnny-code <f.codecco.grando@gmail.com>
2025-01-16 18:13:32 +0100

    update progressbars

--- js/components/progressBar.js
+++ js/components/progressBar.js
@@ -1,10 +1,32 @@
+export function createProgressBars(skills) {
+  const container = document.querySelector(".skills-container");
+
+  skills.forEach((skill, index) => {
+    // Ajouter un titre de séparation avant les 3 dernières compétences (Webdesign)
+    if (index === skills.length - 3) {
+      const separator = document.createElement("div");
+      separator.classList.add("separator");
+      container.appendChild(separator);
+    }
+
+    // Créer une barre de progression pour chaque compétence
+    const progressBar = createProgressBar(skill);
+    container.appendChild(progressBar);
+  });
+}
+
 export function createProgressBar(skill) {
   // Vérification que l'objet skill est bien défini
-  if (!skill || !skill.name || !skill.percentage || !skill.icon) {
+  if (
+    !skill ||
+    !skill.name ||
+    !skill.percentage ||
+    (!skill.icon && !skill.image)
+  ) {
     throw new Error(`Invalid skill object: ${JSON.stringify(skill)}`);
   }
 
-  const { name, percentage, icon } = skill;
+  const { name, percentage, icon, image } = skill;
 
   // Création du conteneur principal
   const progressBarContainer = document.createElement("div");
@@ -18,13 +40,17 @@ export function createProgressBar(skill) {
     "bg-body",
     "rounded"
   );
+  // Génération du contenu pour les icônes ou images
+  const mediaElement = icon
+    ? `<i class="${icon} fa-2x me-3 text-teal"></i>` // Icône Font Awesome
+    : `<img src="${image}" alt="Icone de ${name}" class="me-3 ${name === "Adobe" ? "bg-teal rounded-1" : ""}" style="width: 25px;">`; // Image
 
   // Ajout du contenu HTML
   progressBarContainer.innerHTML = `
         <div class="row align-items-center">
             <div class="col-5 col-sm-4 col-md-3 col-xl-2">
                 <div class="d-flex align-items-center">
-                    <i class="${icon} fa-2x me-3 text-teal"></i>
+                    ${mediaElement}
                     <p class="fw-bold m-0">${name}</p>
                 </div>
             </div>
--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 import { skills } from "./data/skills.js";
-import { createProgressBar } from "./components/progressBar.js";
+import { createProgressBars } from "./components/progressBar.js";
 import { portfolioData } from "./data/portfolioData.js";
 import { createProjectCard, createProjectModal } from "./components/card.js";
 
 
 // Ciblez le conteneur des compétences
 const skillsContainer = document.querySelector(".skills-container");
-
-skills.forEach((skill) => {
-  const progressBar = createProgressBar(skill);
-  skillsContainer.appendChild(progressBar);
-});
+// Générer toutes les barres de progression avec séparation
+createProgressBars(skills);
 
 const portfolioContainer = document.querySelector("#portfolio .row");
 
